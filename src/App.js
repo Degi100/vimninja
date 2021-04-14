f0331fc1528b93a1b2001a5362d634ed84997620
Rene Degering <73115856+Degi100@users.noreply.github.com>
2021-04-14 15:50:51 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Test
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React 1000000000000000000%
         </a>
       </header>
     </div>

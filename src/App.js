bdbae53c2cdcb6484a4491fb2478bb9ab50aeb69
Rene Degering <73115856+Degi100@users.noreply.github.com>
2021-04-14 16:53:48 +0200

    add -!

--- src/App.js
+++ src/App.js
@@ -4,19 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className="App">
         <p>
-          Test
+          Never change the running system
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React 1000000000000000000%
-        </a>
       </header>
     </div>
   );

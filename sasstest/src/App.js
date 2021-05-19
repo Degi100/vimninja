69219884b1349dde824347cefaa749d31f569527
Degi100 <r.degering@mail.de>
2021-05-19 19:46:59 +0200

    sasstest

---
+++ sasstest/src/App.js
@@ -0,0 +1,21 @@
+import './App.scss';
+
+function App() {
+return (
+<div className="App">
+    <header>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+
+        </nav>
+        <h1>Degidev</h1>
+    </header>
+</div>
+);
+}
+
+export default App;
